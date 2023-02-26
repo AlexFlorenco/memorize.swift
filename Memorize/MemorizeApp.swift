d4826e92cb601e5949067dbc9c478919c101826c
Alexandre Floren;o <aflorenco13@gmail.com>
2023-02-26 11:09:05 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Alexandre Florenço on 26/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
